05b03bcfe1f9eef45f14b683494f34c768ee8a59
Irina Borisova <i.a.borisova@gmail.com>
2022-09-02 22:53:18 +0200

    Exercises loaded with student and lecture instances

---
+++ students-csv-master/exercises.rb
@@ -0,0 +1,16 @@
+
+class Exersise
+  attr_reader :student_id, :lecture_id,:exercise_name, :percentage_achieved
+  def initialize(exer_student, exer_lecture, exercise_name, percentage_achieved)
+    @exer_student = exer_student
+    @exer_lecture = exer_lecture
+    @student_id = @exer_student.student_id
+    @lecture_id = @exer_lecture.lecture_id
+    @exercise_name = exercise_name
+    @percentage_achieved = percentage_achieved
+  end
+
+  def print_info
+    puts "The exercise #{@exercise_name} form the lecture #{@exer_lecture.title} is done by a student #{@exer_student.full_name} to a percentage #{@percentage_achieved}"
+  end
+end
--- students-csv-master/read_csv.rb
+++ students-csv-master/read_csv.rb
@@ -1,13 +1,14 @@
 require_relative 'students'
 require_relative 'lectures'
+require_relative 'exercises'
 
 students_csv_content = File.read('students.csv')
 # puts students_csv_content
-lines = students_csv_content.split("\n")
+student_lines = students_csv_content.split("\n")
 
 students = []
 
-lines[1..-1].each do |line|
+student_lines[1..-1].each do |line|
   row = line.split(",")
   student_id = row[0]
   full_name = row[1]
@@ -33,4 +34,32 @@ end
 
 lectures.each do |lecture|
   lecture.print_info
+end
+
+exercise_csv_content = File.read('exercises.csv')
+exercise_lines = exercise_csv_content.split("\n")
+
+exercises = []
+
+exercise_lines[1..-1].each do |line|
+  row = line.split(",")
+  exer_student = ""
+  students.each do |student|
+    if row[0] == student.student_id
+      exer_student = student
+    end
+  end
+  exer_lecture = ""
+  lectures.each do |lecture|
+    if row[1] == lecture.lecture_id
+      exer_lecture = lecture
+    end
+  end
+  exercise_name = row[2]
+  percentage_achieved = row[3]
+  exercises << Exersise.new(exer_student, exer_lecture, exercise_name, percentage_achieved)
+end
+
+exercises[0..20].each do |exercise|
+  exercise.print_info
 end
